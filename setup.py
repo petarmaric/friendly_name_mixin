ffe0781a063e1086df9fb4f66b1e58dd37468dab
Petar Marić <petar.maric@gmail.com>
2022-09-04 18:21:35 +0200

    Ported project to Python 3

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-import sys
 from setuptools import setup
-
-
-if sys.version_info < (2, 6):
-    print 'ERROR: friendly_name_mixin requires at least Python 2.6 to run.'
-    sys.exit(1)
 
 
 setup(
@@ -21,14 +15,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development',
         'Topic :: Utilities',
     ],
     platforms='any',
     py_modules=['friendly_name_mixin'],
-    setup_requires = ['nose>=1.0'],
     tests_require = ['coverage>=3.3', 'nose>=1.0', 'nosexcover>=1.0.7'],
     test_suite = 'nose.collector',
 )
